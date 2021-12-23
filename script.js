8d63732e7d4d1d4db75061e879b6a712866a960c
rashida-otunba <rashidaotunba@gmail.com>
2021-12-22 22:48:14 -0500

    updated CSS and JS files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let getContainerDiv=document.getElementById("container");
 
 let headingElement=document.createElement("h1");
-headingElement.textContent="There are twelve zodiac signs";
+headingElement.textContent="ZODIAC SIGNS";
 getContainerDiv.appendChild(headingElement);
 headingElement.classList.add("header-class");
 
@@ -10,16 +10,82 @@ ariesElement.textContent="Aries";
 getContainerDiv.appendChild(ariesElement);
 ariesElement.classList.add("zodiac-list");
 
-let aries_para=document.createElement("h4");
-aries_para.textContent="i am Aries sub content";
-ariesElement.appendChild(aries_para);
+// let aries_para=document.createElement("h4");
+// aries_para.textContent="i am Aries sub content";
+// ariesElement.appendChild(aries_para);
 
 let taurusElement=document.createElement("h3");
 taurusElement.textContent="Taurus";
 getContainerDiv.appendChild(taurusElement);
 taurusElement.classList.add("zodiac-list");
 
-let taurus_para=document.createElement("h3");
+let geminiElement=document.createElement("h3");
+geminiElement.textContent="Gemini";
+getContainerDiv.appendChild(geminiElement);
+geminiElement.classList.add("zodiac-list");
+
+let cancerElement=document.createElement("h3");
+cancerElement.textContent="Cancer";
+getContainerDiv.appendChild(cancerElement);
+cancerElement.classList.add("zodiac-list");
+
+let leoElement=document.createElement("h3");
+leoElement.textContent="Leo";
+getContainerDiv.appendChild(leoElement);
+leoElement.classList.add("zodiac-list");
+
+let virgoElement=document.createElement("h3");
+virgoElement.textContent="Virgo";
+getContainerDiv.appendChild(virgoElement);
+virgoElement.classList.add("zodiac-list");
+
+let libraElement=document.createElement("h3");
+libraElement.textContent="Libra";
+getContainerDiv.appendChild(libraElement);
+libraElement.classList.add("zodiac-list");
+
+let scorpioElement=document.createElement("h3");
+scorpioElement.textContent="Scorpio";
+getContainerDiv.appendChild(scorpioElement);
+scorpioElement.classList.add("zodiac-list");
+
+let sagElement=document.createElement("h3");
+sagElement.textContent="Sagittarius";
+getContainerDiv.appendChild(sagElement);
+sagElement.classList.add("zodiac-list");
+
+let capElement=document.createElement("h3");
+capElement.textContent="Capricorn";
+getContainerDiv.appendChild(capElement);
+capElement.classList.add("zodiac-list");
+
+let aquaElement=document.createElement("h3");
+aquaElement.textContent="Aquarius";
+getContainerDiv.appendChild(aquaElement);
+aquaElement.classList.add("zodiac-list");
+
+let piscesElement=document.createElement("h3");
+piscesElement.textContent="Pisces";
+getContainerDiv.appendChild(piscesElement);
+piscesElement.classList.add("zodiac-list");
+
+
+
+
+
+// Aries (March 21 – April 19)
+// Taurus (April 20 – May 20)
+// Gemini (May 21 – June 20)
+// Cancer (June 21 – July 22)
+// Leo (July 23 – August 22)
+// Virgo (August 23 – September 22)
+// Libra (September 23 – October 22)
+// Scorpio (October 23 – November 21)
+// Sagittarius (November 22 – December 21)
+// Capricorn (December 22 – January 19)
+// Aquarius (January 20 – February 18)
+// Pisces (February 19 – March 20)
+
 
 
 
